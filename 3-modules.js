1e13bcdc66189ca0b4ed34f79ce9e5f43580fdb2
iamtomlex <tomlex3100@gmail.com>
2023-01-10 06:09:08 -0800

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+// console.log(names)
+const data = require('./6-alternative-flavour')
+require('./7-mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
